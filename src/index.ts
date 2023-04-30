6ac37a9c7a9ded17838682bb9490d45d2b1e2e6a
on22ac <acelya.cagan@gmail.com>
2023-04-30 15:00:55 +0200

    Module angelegt

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+import {ListEintrag } from "./interface";
+
+const liste: Array<ListEintrag> = [
+    {
+      artikel: "Lebensmittel",
+      menge: "Anzahl (entweder Emojis oder Stückzahl angeben)",
+    },
+  ];
