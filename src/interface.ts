79628ff1df2e58d50d7dd0e8f21cff12d264efaa
on22ac <acelya.cagan@gmail.com>
2023-04-30 14:58:05 +0200

    Bauplan(interface) angelegt

---
+++ src/interface.ts
@@ -0,0 +1,8 @@
+//	Typscript spezifische codes werden hier abgelegt
+//Hier stelle ich dar wie meine Listeinträge aufgebaut sind
+//Das ist mein Bauplan
+
+export interface ListEintrag {
+    artikel: string;
+    menge: string;
+  }
