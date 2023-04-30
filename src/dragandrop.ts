0a79956c2f2339d9d0c88cfff0d6f8bff93ffc34
on22ac <acelya.cagan@gmail.com>
2023-04-30 17:28:59 +0200

    DragEnd hinzugefügt

--- src/dragandrop.ts
+++ src/dragandrop.ts
@@ -6,3 +6,10 @@ import { DragTarget } from "./interface";
 const dragStart = (target: DragTarget): void => {
     target.addClass("dragging");
   };
+
+  //wird aufgerufen, wenn der Benutzer das Element loslässt. 
+// entfernt die CSS-Klasse "dragging" aus dem Element, um es nicht länger als gezogen zu markieren.
+const dragEnd = (target: DragTarget): void => {
+    target.removeClass("dragging");
+  };
+  
