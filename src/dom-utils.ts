98aaecc336924af0ba2043a3cbbe9e00ee4b1c7e
on22ac <acelya.cagan@gmail.com>
2023-04-30 17:11:03 +0200

    QuerySelektorAll für const cards angelegt

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -2,3 +2,6 @@ export const listContainer = document.querySelector("#listContainer") as HTMLOLi
 export const inputArtikel = document.querySelector("#artikel") as HTMLInputElement;
 export const inputMenge = document.querySelector("#menge") as HTMLInputElement;
 export const addBtn = document.querySelector("#addBtn") as HTMLButtonElement;
+
+//const cards wählt alle HTML-Elemente aus, die der CSS-Klasse "card" entsprechen und speichert sie in einer Liste mit dem Namen "cards".
+const cards = document.querySelectorAll<HTMLElement>(".card");
