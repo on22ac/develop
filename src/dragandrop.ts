53006392352b191482e7eb3a36a652947e4ff469
on22ac <acelya.cagan@gmail.com>
2023-05-05 12:24:12 +0200

    weitere Kommentare für dragandrop.ts hinzugefügt

--- src/dragandrop.ts
+++ src/dragandrop.ts
@@ -38,19 +38,21 @@ const dragLeave = (event: DragEvent): void => {
 // "setData" wird verwendet, um den HTML-Code und die "id" des gezogenen Elements im "dataTransfer"-Objekt zu speichern. 
 //Später können diese Daten dann in der "drop"-Funktion abgerufen werden, um das gezogene Element entsprechend zu manipulieren oder zu platzieren.
  
-const drag = (event: DragEvent): void => {
-    const dataTransfer = event.dataTransfer as DataTransfer;
-    dataTransfer.setData(
-      "text/html",
-
 //"text/html:  gibt den Typ der Daten an, die im "dataTransfer"-Objekt gespeichert werden sollen.
 //hier wird HTML-Code des gezogenen Elements als Daten übertragen. Daher wird der Typ der Daten als "text/html" angegeben, um zu signalisieren, dass es sich um HTML-Code handelt.
 // der HTML-Code im "dataTransfer"-Objekt kann als formatierter Text gespeichert wird, der später wieder als HTML-Code verwendet werden kann, wenn das Element abgelegt wird.
-      
+
+// currentTarget: . Es ist das aktuelle HTML-Element, auf dem ein Ereignislistener registriert wurde und das das Ereignis verarbeitet. Auch wenn es andere HTML-Elemente auf der Seite gibt, 
+//konzentriert sich der Ereignislistener auf das aktuelle Element, das das Ereignis verarbeitet.
+const drag = (event: DragEvent): void => {
+    const dataTransfer = event.dataTransfer as DataTransfer;
+    dataTransfer.setData(
+      "text/html",    
     (event.currentTarget as HTMLElement).outerHTML
     );
-    const id: string | undefined = (event.currentTarget as HTMLElement).dataset
-      .id;
+
+    //dataset.id wird verwendet um die id des Elements zu finden, welches gerade gezogen wird welches dann dem dataransfer übergeben wird 
+    const id: string | undefined = (event.currentTarget as HTMLElement).dataset.id;
     if (id !== undefined) {
       dataTransfer.setData("text/plain", id);
     }
@@ -59,17 +61,27 @@ const drag = (event: DragEvent): void => {
   function drop(event: DragEvent): void {
     const dataTransfer = event.dataTransfer as DataTransfer;
     const id: string = dataTransfer.getData("text/plain");
-    const elementToRemove: HTMLElement | null = document.querySelector(
-      `[data-id="${id}"]`
-    );
+
+    //es kann entweder ein HTMLElement objekt speichren oder dne Wert null
+    //querySelector:  sucht nach einem  Element im HTML-Dokument, das das Attribut data-id hat, 
+    //das dem Wert der id-Variable entspricht, die aus den übergebenen Drag-and-Drop-Daten abgerufen wurde.
+    // wenn kein passendes Element gefunden wurde wird 0 elementToRemove zugewiesen.
+    const elementToRemove: HTMLElement | null 
+    = document.querySelector(`[data-id="${id}"]`);
     
+
+    // Die if-Anweisung überprüft, ob das Element elementToRemove existiert.
+    // Wenn es existiert, wird die remove()-Methode aufgerufen, um das Element aus dem Dokument zu entfernen.
     if (elementToRemove) {
       elementToRemove.remove();
     }
-    
+    // event.preventDefault verhindert, dass der Browser eine Standardaktion für das Drop-Ereignis ausführt. 
+    //Ohne diese Anweisung würde der Browser versuchen, den Inhalt der Datei anzuzeigen, die gezogen wurde.
     event.preventDefault();
     
     const target = event.target as HTMLElement;
+    //Der Code sucht nach dem nächsten Element auf der Webseite, das die Klasse 'column' hat, 
+    //und weist das Ergebnis dieser Suche der Variable parent zu. Dadurch kann das Ziel-Element an der richtigen Stelle in der HTML-Struktur platziert werden.
     const parent = target.closest('.column');
     
     if (target.classList.contains("card")) {
